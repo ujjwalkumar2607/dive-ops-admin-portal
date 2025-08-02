15b63e5a29241e6b47d7c6a6a3bb82cce3488530
Ujjwal Kumar <ujjwalkumar2607@gmail.com>
2025-08-02 17:29:33 -0400

    reverting

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,  // ✅ use import.meta.env
-// });
-
-const API = import.meta.env.VITE_API_URL;
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,  // ✅ use import.meta.env
+});
 
 export const getTest = () => API.get('/api/test');
 export const login = (form) => API.post('/api/login', form);
