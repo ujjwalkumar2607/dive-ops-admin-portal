ae9e5d6f838f97103fe455c8ae79fab877c78aa8
Ujjwal Kumar <ujjwalkumar2607@gmail.com>
2025-08-02 17:24:28 -0400

    testing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,  // ✅ use import.meta.env
-});
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,  // ✅ use import.meta.env
+// });
+
+const API = import.meta.env.VITE_API_URL;
 
 export const getTest = () => API.get('/api/test');
 export const login = (form) => API.post('/api/login', form);
